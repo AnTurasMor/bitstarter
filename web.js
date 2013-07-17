ad318f6fb0bac2533c26786fbe80333b3c3fc049
John Smith <example@stanford.edu>
2013-07-17 22:34:49 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
-var txtout=fs.readFileSync("index.html").buf.toString();
+var txtout=fs.readFileSync("index.html").toString();
 
 app.get('/', function(request, response) {
   response.send(txtout);
